1d726b816cf94d305e2437492e445031f2c72bd1
Shubham Gurav <shubhzgurav33@gmail.com>
2022-05-31 18:27:18 +0530

    above

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // publicPath: "/<restrestaurant_project>/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/restrestaurant_project/'
+    : '/',
 })
